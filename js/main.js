7cdc861b6da2cfae06d21825a90be9cff8416ccb
danipetro21 <idany346@gmail.com>
2022-11-21 10:06:36 +0100

    test

--- js/main.js
+++ js/main.js
@@ -6,18 +6,28 @@
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 const btn = document.getElementById("btn");
 const numeri = document.querySelector(".numeri");
+let seconds = 30;
+
 btn.addEventListener("click",
     function () {
+        //stampo a video i numeri casuali
+        numeri.innerHTML += genArrMinMax(5, 1, 50);      
+        //creo timer
+        //dopo il timer compare input per l'utente
+        //verifico se i numeri sono all'interno dell'array dei numeri generati
+        //restituisco il risultato
 
-            numeri.innerHTML += genArrMinMax(5, 1, 50);
-        
     });
 
+
+
 // FUNZIONE GENERA NUMERI CASUALI
+
 function randomInteger(min, max) {
     return (Math.floor(Math.random() * ((max + 1) - min)));
 }
 // ARRAY 5 NUMERI CASUALI
+
 function genArrMinMax(quanti, minNum, maxNum) {
 
     const arrayRandom = [];
@@ -34,8 +44,18 @@ function genArrMinMax(quanti, minNum, maxNum) {
 }
 
 
+// // FUNZIONE RIPROVA
+
+// function riprova {
+//     window.location.reload();
+// }
+
 // FUNZIONE TIMER
 
+function timer(second) {
+    
+    second--;
 
+}
 
 
